e24194eb2ba7fbea0a04fdbd5d700af1e1a0563b
RTM <rtm@RTMs-MacBook-Air.local>
2025-08-25 17:46:11 +0530

    a

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,21 @@ import Navbar from './components/Navbar';
 import { BrowserRouter , Routes, Route } from "react-router-dom";
 import './App.css'
 import Footer from './components/Footer';
+import { HashRouter } from 'react-router-dom';
 
 
 function App() {
 
   return (
     
-      <BrowserRouter>
+      <HashRouter>
         <Navbar  />
       <Routes>
         <Route path="/" element={<Holidays />} />
         <Route path="/holidays" element={<Holidays />} />
       </Routes>
         <Footer/>
-    </BrowserRouter>
+    </HashRouter>
    
   )
 }
